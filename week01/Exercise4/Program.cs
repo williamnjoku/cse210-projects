9e425a14bc80519ec3d8c0923ac876737ec08f88
williamnjoku <njokuwilliam19@gmail.com>
2025-03-06 00:07:54 +0100

    Finished prep 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -8,7 +8,7 @@ class Program
         List<int> numbers = new List<int>();
         int sum = 0;
         int maxNumber = 0;
-        int smallestNumber = 999999;
+        int minNumber = 999999;
 
         Console.WriteLine("Enter a series of numbers, positive or negative(type 0 to finish):");
 
@@ -26,8 +26,8 @@ class Program
             if (number > maxNumber)
                 maxNumber = number;
 
-            if (number < smallestNumber && number > 0)
-                smallestNumber = number;
+            if (number < minNumber && number > 0)
+                minNumber = number;
         }
 
         if (numbers.Count > 0) 
@@ -37,7 +37,7 @@ class Program
             Console.WriteLine($"The sum is: {sum}");
             Console.WriteLine($"The average is: {average:F2}");
             Console.WriteLine($"The maximum number is: {maxNumber}");
-            Console.WriteLine($"The smallest positive number is: {smallestNumber}");
+            Console.WriteLine($"The smallest positive number is: {minNumber}");
 
             Console.WriteLine("The sorted list is: ");
 
