a428129a4a919adc5b3510dc178f1078df263926
williamnjoku <njokuwilliam19@gmail.com>
2025-03-05 23:03:40 +0100

    Finished Prep Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes"; 
+        
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(1,101);
+
+            int guessNumber = 0;
+            int attempts = 0;
+
+            while (guessNumber != number)
+            {
+                Console.Write("Guess the magic number: ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+
+                attempts++;
+
+                if (guessNumber > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            }
+            
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {attempts} attempts.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+        Console.WriteLine("Thank you for playing.");
     }
 }
