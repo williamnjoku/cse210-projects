82c958ac5bfce79648490e882198223280f8af12
williamnjoku <njokuwilliam19@gmail.com>
2025-03-05 13:32:22 +0100

    Adjusted the variable convention

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
 }
