c40fd056127b145eaaaa8c0ef4f9d2b0988bf50f
williamnjoku <njokuwilliam19@gmail.com>
2025-03-09 20:18:37 +0100

    Abstraction exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        
+        Job job1 = new Job();
+        job1._company = "Amazon";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2020;
+        
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Data Analyst";
+        job2._startYear = 2015;
+        job2._endYear = 2023;
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+
+        myResume.DisplayResume();
+
+
     }
 }
