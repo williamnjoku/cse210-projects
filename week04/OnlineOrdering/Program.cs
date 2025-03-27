2991a5c593e4605c885b554e249f9287e76f28c5
williamnjoku <njokuwilliam19@gmail.com>
2025-03-27 22:23:36 +0100

    Added the screenshot

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -14,7 +14,7 @@ class Program
         Order order2 = new Order(customer2);
 
         order1.AddProduct(new Product("Laptop", 101, 999.99f, 1));
-        order1.AddProduct(new Product("Mouse", 102, 49.99f, 2));
+        order1.AddProduct(new Product("Computer mouse", 102, 49.99f, 2));
 
         order2.AddProduct(new Product("Monitor", 103, 199.99f, 1));
         order2.AddProduct(new Product("Keyboard", 104, 89.99f, 1));
